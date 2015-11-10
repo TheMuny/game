36c8b2c43525cc2d19af09eda1b8578396694210
Munir <gm.BlackGod@gmail.com>
2015-11-10 15:24:44 +0200

    test

--- api/model/src/com/game/api/service/impl/GameService.java
+++ api/model/src/com/game/api/service/impl/GameService.java
@@ -10,30 +10,40 @@ import com.game.api.model.monster.impl.FlyingMonster;
 import com.game.api.model.monster.impl.HeroPlayer;
 import com.game.api.model.monster.impl.WalkingMonster;
 
+
 public class GameService implements com.game.api.service.GameService{
 
-//	private Character [][]map= new Character[25][25];
+	private Character [][]map= new Character[25][25];
 	private Monster[]monsters = new Monster[10];
 	private HeroPlayer hero = new HeroPlayer();
-	private Point p = new Point();
+	private Point p;
 	
 	
 	public void setLocationCell(){		
 		for(int i =0;i<monsters.length;i++){
 			if(i%2==0){
+				Point s = new Point();
 				Random r = new Random();
-				p.setLocation(r.nextInt(25),r.nextInt(25));
-				monsters[i]=new WalkingMonster(60,p);			
+				s.setLocation(r.nextInt(25),r.nextInt(25));
+				monsters[i]=new WalkingMonster(60,s);			
 			}else{
-				 Random r = new Random();
-				p.setLocation(r.nextInt(25),r.nextInt(25));
-				monsters[i]=new FlyingMonster(25,p);
+				Point m = new Point();
+				Random r = new Random();
+				m.setLocation(r.nextInt(25),r.nextInt(25));
+				monsters[i]=new FlyingMonster(25,m);
 			}
 		}		
 	}
 	
 	
-	
+	public void showOnMap(){
+    	Character [][]map1 = new Character[25][25];
+    	for (int i=0;i<monsters.length;i++){
+    		 Movable m = (Movable) monsters[i];
+    		map1[m.getPosition().x][m.getPosition().y]= (Character) m;
+    	}
+    	map=map1;
+	}
 	
 	
 	@Override
@@ -47,6 +57,7 @@ public class GameService implements com.game.api.service.GameService{
 	}
 
 	public Monster getMonster(int index) {
+	
 		return monsters[index];
 	}
 	@Override
@@ -55,11 +66,9 @@ public class GameService implements com.game.api.service.GameService{
 	    for(int i = 0; i < monsters.length; i++) {            	
                 Movable m = (Movable) monsters[i];
                 Random r = new Random();
-                m.moveTo(new Point(r.nextInt(25),r.nextInt(25)));
-                
+                m.moveTo(new Point(r.nextInt(25),r.nextInt(25)));               
                 monsters[i]= (Monster) m;
-        }
-		
+        }	
 	}
 
 
@@ -68,9 +77,32 @@ public class GameService implements com.game.api.service.GameService{
 
 		
 		GameService G = new GameService();
-		G.setLocationCell();
+		G.setLocationCell();//test
+//		for (int i=0;i<G.monsters.length;i++){
+//			if(G.monsters[i]!=null){
+//				Movable m = (Movable) monsters[i];
+//				int x = m.getPosition().x;
+//				int y = m.getPosition().y;
+//				System.out.println(x+" "+y);
+//			}else{
+//				System.out.println(i+" ");
+//			}
+//		}
+//		G.calculateNextStep();
 		
-		G.calculateNextStep();
+		
+		G.showOnMap();//test
+		for (int i=0;i<G.map.length;i++){
+			for (int j=0;j<G.map[i].length;j++){
+				if(G.map[i][j]==null){
+					
+					System.out.println(i+" "+j+" ");
+				}else{
+					System.out.println(i+" "+j+" "+" - Monster");
+				}
+			}
+		}
+		
 		
 		
 //		Point p   = new Point();
